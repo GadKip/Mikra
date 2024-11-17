5da2cb56a026099a71bc557f23d3794a8fe28e41
GadKip <gadavihai@gmail.com>
2024-11-17 17:36:04 +0200

    strating to style, nativewind...

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,17 @@
-import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import "../global.css";
+
 
 
 export default function App() {
   return (
-    <View className="flex-1 items-right justify-center bg-green">
-      <Text className="text-3xl">מקרא מבואר</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" className='text-3xl items-right'>Go to Profile</Link>
+    <View>
+      <Text className="color-orange-800 bg-sky-500">מקרא מבואר</Text>
+      <StatusBar />
+      <Text className="text-3xl">ספר</Text>
+      <Link href="/profile">ספר</Link>
     </View>
   );
 }
